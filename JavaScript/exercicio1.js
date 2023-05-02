5ff6964b5511404949d843fb062d5e68005716c2
matheusfonseca18 <matheus.fonseca0218@gmail.com>
2023-05-02 11:53:18 -0300

    primeira aula de JS

---
+++ JavaScript/exercicio1.js
@@ -0,0 +1,28 @@
+console.log("Olá Mundo!");
+
+console.log("--Exercicio 1--");
+var numero1=70;
+var numero2=20;
+console.log("soma de", numero1, "+", numero2, "é igual a", numero1 + numero2);
+
+console.log();
+
+console.log("--Exercicio 2--");
+var palavra1="mouse";
+var palavra2="teclado";
+console.log(palavra1, "e", palavra2);
+
+console.log();
+
+console.log("--Exercicio 3--");
+var celular = true // false;
+console.log(`eu tenho celulat? ${celular}`);
+
+console.log();
+
+console.log("--Exercicio 4--");
+var nota = 8;
+var nome = "Matheus";
+var sobrenome = "Fonseca";
+console.log("O aluno", nome, sobrenome, "tirou nota", nota);
+console.log(`Esse aluno ${nome} ${sobrenome} tirou a nota ${nota} na prova`);
