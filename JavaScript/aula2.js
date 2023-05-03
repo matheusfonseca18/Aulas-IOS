c0fd8f50badffec0cc555a32d9ad43eacb2cd85a
matheusfonseca18 <matheus.fonseca0218@gmail.com>
2023-05-03 09:59:44 -0300

    js 2

---
+++ JavaScript/aula2.js
@@ -0,0 +1,37 @@
+var numerosQuebrado=7.8
+var numDividindo=10/2
+var letras="letras aqui"
+var infinito= Infinity
+
+console.log(numDividindo)
+console.log(numerosQuebrado)
+console.log(typeof numDividindo)
+console.log(letras)
+console.log(typeof letras)
+console.log(infinito)
+console.log(typeof infinito)
+
+console.log(`aaaaaaa ${letras} \n numeros ${numerosQuebrado}`)
+console.log(`aaaaaaa ${letras} \t\t\t\t\t\t\t numeros ${numerosQuebrado}`)
+
+var media= 8 >= 7
+console.log(media)
+
+var sub ="5" - 1
+console.log(sub)
+
+console.log("calculo de media")
+
+var notas= (8 + 6+ 7)/3
+console.log(notas)
+
+console.log(3 == "8")
+console.log(3 === "8")
+
+console.log(`a média da note foi de ${notas}`)
+
+console.log(typeof "x" && 4.5)
+
+var freq=4
+
+console.log (`a media foi de ${}`)
